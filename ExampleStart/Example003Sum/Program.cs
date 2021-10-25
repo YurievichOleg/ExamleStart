41265a7eeaa01d91c819929b08b266f60ff5fe7a
Good Day <goodday@MacBook-Pro-Good.local>
2021-10-25 03:20:35 +0300

    new Random

--- ExampleStart/Example003Sum/Program.cs
+++ ExampleStart/Example003Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int nomberA = 83;
-int nomberB = 5;
+﻿int nomberA = new Random().Next(1,10);// 1,2,3....9
+Console.WriteLine(nomberA);
+int nomberB = new Random().Next(10,20);// 10,11,12...19
+Console.WriteLine(nomberB);
 int result = nomberA + nomberB;
 Console.WriteLine(result);
