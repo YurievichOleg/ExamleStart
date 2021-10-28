4522726d15949f1950b7766bed6095384772003b
Good Day <goodday@MacBook-Pro-Good.local>
2021-10-28 14:29:29 +0300

    Сделали задачу с поиском элемента по значению

---
+++ ExampleStart/Exemple010ZadachaMassiv/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1,12,35,88,45,56,33,88,123,555,888};
+
+int n = array.Length;
+
+int find = 88;
+
+int index = 0; 
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
